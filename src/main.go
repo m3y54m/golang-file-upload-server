9274710a81364a617c5478706b05291cac377990
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-07-29 17:28:30 +0430

    Create a "hello world" http server

--- src/main.go
+++ src/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {
-	// hello world
-	fmt.Println("Hello World")
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+	})
+	http.ListenAndServe(":8080", nil)
+}
 
+func main() {
+	fmt.Println("Simple Go File Upload Server")
+	setupRoutes()
 }
